a927200c81bbd773ad6bed7334f1130dca53fdbd
Hasse <carlos@hassetech.com>
2023-11-23 16:38:00 -0300

    Designer completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
+import ListaSuspensa from "./componentes/ListaSuspensa";
 
 function App() {
   return (
